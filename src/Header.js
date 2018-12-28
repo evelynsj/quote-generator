e71d8da87481b844ab7edbbea5b0a46b33aaa627
Evelyn Sjafii <evelynsj98@gmail.com>
2018-12-28 20:32:51 +0700

    Files Setup

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <h1>Header Section</h1>
+    );
+}
+
+export default Header
---
+++ src/QuoteGenerator.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class QuoteGenerator extends React.Component {
+    render() {
+        return (
+            <h1>Quote Generator Section</h1>
+        );
+    }
+}
+
+export default QuoteGenerator
